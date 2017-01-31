879b996303834639cfbd0c36cff3ee5407d24bc8
omar Abu-baker <omar Abu-baker>
2017-01-30 23:16:25 -0800

    Completed touch screen and implemented bluetooth; bluetooth needs more testing

--- exercises/ex1.3-4/ex1.3-redo/software/exfour/hello_world.c
+++ exercises/ex1.3-4/ex1.3-redo/software/exfour/hello_world.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#define USLEEP_SEC 1000000
 
 #define RS232_CONTROL 	(*(volatile unsigned char *) (0x84000200))
 #define RS232_STATUS 	(*(volatile unsigned char *) (0x84000200))
@@ -12,87 +16,78 @@
 #define TOUCH_RXDATA 	(*(volatile unsigned char *) (0x84000232))
 #define TOUCH_BAUD 		(*(volatile unsigned char *) (0x84000234))
 
-/* a data type to hold a point/coord */
+#define BT_CONTROL 	(*(volatile unsigned char *) (0x84000220))
+#define BT_STATUS 	(*(volatile unsigned char *) (0x84000220))
+#define BT_TXDATA 	(*(volatile unsigned char *) (0x84000222))
+#define BT_RXDATA 	(*(volatile unsigned char *) (0x84000222))
+#define BT_BAUD 	(*(volatile unsigned char *) (0x84000224))
+
+/* a data type to hold a point/coordinate */
 typedef struct {
 	int x, y;
 } Point;
 
-// RS232 functions
-void Init_RS232(void);
-int putcharRS232(int c);
-int getcharRS232(void);
-int RS232TestForReceivedData(void);
-
 // Touch screen functions
-void Init_Touch(void);
-int ScreenTouched(void);
-void WaitForTouch();
-Point GetPress(void);
-Point GetRelease(void);
+void initTouch(void);
+int screenTouched(void);
+void waitForTouch();
+Point getPress(void);
+Point getRelease(void);
+
+// General purpose functions
+int getChar(volatile unsigned char* status, volatile unsigned char* rx);
+int putChar(volatile unsigned char* status, volatile unsigned char* tx, int c);
+// sends a string to the serial tx, string must be terminated by \0, \0 is not sent
+void putString(volatile unsigned char* status, volatile unsigned char* tx,
+		char *string);
+
+// Bluetooth functions
+void initBluetooth(void);
+void setBluetoothName(char *name);
+void setBluetoothPassword(char *password);
 
 int main() {
 	printf("Hello from Nios II!\n");
 
-	//Init_RS232();
-	Init_Touch();
+	initBluetooth();
 
-	Point p;
-	while(1) {
-		p = GetPress();
-		printf("%d, %d", p.x, p.y);
-		p = GetRelease();
-		printf("%d, %d", p.x, p.y);
-	}
-
+	/*// use to test touch screen
+	 initTouch();
+	 Point p;
+	 while (1) {
+	 p = getPress();
+	 printf("press %d, %d\n", p.x, p.y);
+	 p = getRelease();
+	 printf("release %d, %d\n", p.x, p.y);
+	 }
+	 */
 	return 0;
 }
 
-// ###########################################################################
-// ###########################################################################
-// ###########################################################################
-
-/**************************************************************************
- Subroutine to initialise the RS232 Port by writing some data
- ** to the internal registers.
- ** Call this function at the start of the program before you attempt
- ** to read or write to data via the RS232 port
- **
- ** Refer to 6850 data sheet for details of registers and
- ***************************************************************************/
-void Init_RS232(void) {
-	// set up 6850 Control Register to utilise a divide by 16 clock,
-	// set RTS low, use 8 bits of data, no parity, 1 stop bit,
-	// transmitter interrupt disabled
-	// program baud rate generator to use 115k baud
-	RS232_CONTROL = 0x55;
-	RS232_BAUD = 0x01;
+int getChar(volatile unsigned char* status, volatile unsigned char* rx) {
+	while (!(*status & 0x1))
+		;
+	int result = *rx;
+	return result & 255;
 }
 
-int putcharRS232(int c) {
-	// poll Tx bit in 6850 status register. Wait for it to become '1'
-	// write 'c' to the 6850 TxData register to output the character
-	while (!(RS232_STATUS & 0x2))
+int putChar(volatile unsigned char* status, volatile unsigned char* tx, int c) {
+	while (!(*status & 0x2))
 		;
-	RS232_TXDATA = c;
+	*tx = c;
 	return c; // return c
 }
 
-int getcharRS232(void) {
-	// poll Rx bit in 6850 status register. Wait for it to become '1'
-	// read received character from 6850 RxData register.
-	while (!(RS232_STATUS & 0x1))
-		;
-	int result = RS232_RXDATA;
-	return result;
-}
-
-// the following function polls the 6850 to determine if any character
-// has been received. It doesn't wait for one, or read it, it simply tests
-// to see if one is available to read
-int RS232TestForReceivedData(void) {
-	// Test Rx bit in 6850 serial comms chip status register
-	// if RX bit is set, return TRUE, otherwise return FALSE
-	return (RS232_STATUS & 0x01);
+void putString(volatile unsigned char* status, volatile unsigned char* tx,
+		char *string) {
+	char c;
+	int i = 0;
+	sendnextchar: c = string[i];
+	if (c == '\0')
+		return;
+	putChar(status, tx, (int) c);
+	i++;
+	goto sendnextchar;
 }
 
 // ###########################################################################
@@ -102,7 +97,7 @@ int RS232TestForReceivedData(void) {
 /*****************************************************************************
  ** Initialise touch screen controller
  *****************************************************************************/
-void Init_Touch(void) {
+void initTouch(void) {
 	// Program 6850 and baud rate generator to communicate with touchscreen
 	// send touchscreen controller an "enable touch" command
 
@@ -111,15 +106,12 @@ void Init_Touch(void) {
 	TOUCH_BAUD = 0x07;
 
 	// now that it is set up, we talk to the touch controller through rx & tx
-	while((TOUCH_STATUS & 0x02) == 0x00);
-	//Wait_Ready_W(TOUCH_STATUS);
-	TOUCH_TXDATA = 0x55;
-	while((TOUCH_STATUS & 0x02) == 0x00);
-	TOUCH_TXDATA = 0x01;
-	while((TOUCH_STATUS & 0x02) == 0x00);
-	TOUCH_TXDATA = 0x12; // TOUCH_ENABLE
+	putChar(&TOUCH_STATUS, &TOUCH_TXDATA, 0x55);
+	putChar(&TOUCH_STATUS, &TOUCH_TXDATA, 0x01);
+	putChar(&TOUCH_STATUS, &TOUCH_TXDATA, 0x12);
 
-	while(TOUCH_RXDATA != 0x00); // wait for OK
+	while (getChar(&TOUCH_STATUS, &TOUCH_RXDATA) != 0)
+		; // wait for OK
 
 	printf("touch screen initialized\n");
 }
@@ -130,72 +122,111 @@ void Init_Touch(void) {
 int ScreenTouched(void) {
 	// return TRUE if any data received from 6850 connected to touchscreen
 	// or FALSE otherwise
-	return ((TOUCH_STATUS & 0x01) == 0x01);
+	return getChar(&TOUCH_STATUS, &TOUCH_RXDATA) == 129;
 }
 
 /*****************************************************************************
  ** wait for screen to be touched
  *****************************************************************************/
-void WaitForTouch() {
-	while (!ScreenTouched()) {
+void waitForTouch() {
+	while (!ScreenTouched())
 		;
-	}
 }
 
 /*****************************************************************************
  * This function waits for a touch screen press event and returns X,Y coord
  *****************************************************************************/
-Point GetPress(void) {
+Point getPress(void) {
+
 	Point p1;
-	// wait for a pen down command then return the X,Y coord of the point
-	// calibrated correctly so that it maps to a pixel on screen
-
 	char bytes[4];
-
-	WaitForTouch();
-	char rxData = TOUCH_RXDATA;
-	while(!((rxData & 128) && (rxData & 1))) { // wait for start of down packet
-		rxData = TOUCH_RXDATA;
-	}
-
+	while (getChar(&TOUCH_STATUS, &TOUCH_RXDATA) != 129)
+		;
 	int i;
-	for (i = 0; i < 4; i++) {
-		while((TOUCH_STATUS & 0x02) == 0x00);
-		bytes[i] = TOUCH_RXDATA;
-	}
-
-	p1.x = bytes[0] + bytes[1];
-	p1.y = bytes[2] + bytes[3];
-
+	for (i = 0; i < 4; i++)
+		bytes[i] = getChar(&TOUCH_STATUS, &TOUCH_RXDATA);
+	p1.x = 480 * ((int) bytes[0] + (int) (bytes[1] << 7)) / 4096.0;
+	p1.y = 800 * ((int) bytes[2] + (int) (bytes[3] << 7)) / 4096.0;
 	return p1;
 }
 
 /*****************************************************************************
  * This function waits for a touch screen release event and returns X,Y coord
  *****************************************************************************/
-Point GetRelease(void) {
+Point getRelease(void) {
+
 	Point p1;
-	// wait for a pen up command then return the X,Y coord of the point
-	// calibrated correctly so that it maps to a pixel on screen
-
 	char bytes[4];
-
-	WaitForTouch();
-	char rxData = TOUCH_RXDATA;
-
-	while(!((rxData & 128) && !(rxData & 1))) { // wait for start of up packet
-		rxData = TOUCH_RXDATA;
-	}
-
+	while (getChar(&TOUCH_STATUS, &TOUCH_RXDATA) != 128)
+		;
 	int i;
-	for (i = 0; i < 4; i++) {
-		while((TOUCH_STATUS & 0x02) == 0x00);
-		bytes[i] = TOUCH_RXDATA;
-	}
-
-	p1.x = bytes[0] + bytes[1];
-	p1.y = bytes[2] + bytes[3];
-
+	for (i = 0; i < 4; i++)
+		bytes[i] = getChar(&TOUCH_STATUS, &TOUCH_RXDATA);
+	p1.x = 480 * ((int) bytes[0] + (int) (bytes[1] << 7)) / 4096.0;
+	p1.y = 800 * ((int) bytes[2] + (int) (bytes[3] << 7)) / 4096.0;
 	return p1;
+}
+
+// ###########################################################################
+// ###########################################################################
+// ###########################################################################
+
+/*****************************************************************************
+ ** Initialise the bluetooth controller
+ *****************************************************************************/
+void initBluetooth(void) {
+
+	// setting up the 6850
+	BT_CONTROL = 0x95;	// 8 bits, no parity, 1 stop bit
+	BT_BAUD = 0x01;	// 115k baud
+	setBluetoothName("Group15");
+	setBluetoothPassword("cpen391");
+	printf("Bluetooth initialized!\n");
+	bluetoothListen();
+}
+
+void setBluetoothName(char *name) {
+	// wait a second before and after entering command mode
+	usleep(USLEEP_SEC);
+	putString(&BT_STATUS, &BT_TXDATA, "$$$"); // enter command mode
+	usleep(USLEEP_SEC);
+
+	// send the command to change the name
+	char command[25] = "SN,";
+	strcat(command, name);
+	strcat(command, "\r\n");
+
+	putString(&BT_STATUS, &BT_TXDATA, command);
+
+
+	usleep(USLEEP_SEC);
+	putString(&BT_STATUS, &BT_TXDATA, "---"); // enter command mode
+	usleep(USLEEP_SEC);
+}
+
+void setBluetoothPassword(char *password) {
+	// wait a second before and after entering command mode
+	usleep(USLEEP_SEC);
+	putString(&BT_STATUS, &BT_TXDATA, "$$$"); // enter command mode
+	usleep(USLEEP_SEC);
+
+	// send the command to change the name
+	char command[25] = "SP,";
+	strcat(command, password);
+	strcat(command, "\r\n");
+
+	putString(&BT_STATUS, &BT_TXDATA, command);
+
+
+	usleep(USLEEP_SEC);
+	putString(&BT_STATUS, &BT_TXDATA, "---"); // enter command mode
+	usleep(USLEEP_SEC);
+}
+
+void bluetoothListen(){
+	while (1){
+		int x = getChar(&BT_STATUS, &BT_RXDATA);
+		printf("%d",x);
+	}
 }
 
