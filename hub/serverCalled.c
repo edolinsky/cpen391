aeb8e85dec8a33080da6310ea7bd34cd3186bac8
Annalies Turley <Annalies Turley>
2017-03-26 22:45:08 -0700

    Hub GUI added

---
+++ hub/serverCalled.c
@@ -0,0 +1,44 @@
+#include "serverCalled.h"
+#include "mainmenu.h"
+
+void drawServerCalled(void){
+	int i;
+	int leftJust = 150;
+	int instrStart = 150;
+	int incr = 15;
+	char title[] = "Your server has been called";
+	char back[] = "Back to main screen";
+
+	for(i = 0; i < 480; i ++){
+		//HLine(int x1, int y1, int length, int Colour)
+		HLine(0, i, 800, GRAY);
+	}
+
+	// Write page title
+	for(i = 0; i < strlen(title); i++){
+		OutGraphicsCharFont2a((400 - strlen(title)*5)+i*10, 200, WHITE, WHITE, title[i], 0);
+	}
+
+	initElements();
+
+	//createElement(int x, int y, int width, int height, int colour);
+
+	// Create three buttons
+	Element *backButton = createElement(100, 350, 600, 100, DIM_GRAY);
+
+	// Set actions
+	setElementAction(backButton, &drawOrderPage);
+
+	// Draw buttons
+	addElementToList(backButton);
+
+	refresh();
+
+
+	// Write back button title
+	for(i = 0; i < strlen(back); i++){
+		OutGraphicsCharFont2a((400 - strlen(back)*5)+i*10, 400, WHITE, WHITE, back[i], 0);
+	}
+
+	listenToTouches();
+}
