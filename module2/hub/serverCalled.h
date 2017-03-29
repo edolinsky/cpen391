48170552ecf3e8bcadafc3e8ae155b655179caa4
Annalies Turley <Annalies Turley>
2017-03-28 20:47:57 -0700

    H files

---
+++ module2/hub/serverCalled.h
@@ -0,0 +1,11 @@
+#include "mainmenu.h"
+#include <stdio.h>
+#include "../serial/touch.h"
+#include "../serial/graphics.h"
+#include "../serial/colours.h"
+#include "todo.h"
+#include "shopping.h"
+#include "orderPage.h"
+#include "../apps/calendar.h"
+
+void drawServerCalled(void);
---
+++ module2/hub/successScreen.h
@@ -0,0 +1,12 @@
+#include "mainmenu.h"
+#include <stdio.h>
+#include "../serial/touch.h"
+#include "../serial/graphics.h"
+#include "../serial/colours.h"
+#include "todo.h"
+#include "shopping.h"
+#include "../apps/calendar.h"
+#include "serverCalled.h"
+#include "orderPage.h"
+
+void drawSuccess(void);
