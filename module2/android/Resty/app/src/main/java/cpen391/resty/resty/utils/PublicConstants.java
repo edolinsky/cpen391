edb1c2df5739341fd6b3ccf5e0e4493ace12a568
reoliveira <oliveira.reid@gmail.com>
2017-04-04 20:25:06 -0700

    public constants

--- module2/android/Resty/app/src/main/java/cpen391/resty/resty/utils/PublicConstants.java
+++ module2/android/Resty/app/src/main/java/cpen391/resty/resty/utils/PublicConstants.java
@@ -5,4 +5,5 @@ public class PublicConstants {
 
     public static final Integer MAX_ORDER = 10;
     public static final Integer MIN_ORDER = 0;
+    public static final String FOR_TABLE = "Table";
 }
